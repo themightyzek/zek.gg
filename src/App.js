5d25146e3fe5011ad37a4d17337b6555869de311
themightyzek <zekromfreak@gmail.com>
2022-01-12 00:57:01 +0100

    holiday work from other laptop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from "./logo.svg";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import { unstable_batchedUpdates } from "react-dom";
 import { ReactComponent as MainLogo } from "./logo/logo.min.svg";
+import projectImage from "./images/li-zhang-8atj_eBENI8-unsplash.jpg";
+import OpenInNew from "@mui/icons-material/OpenInNewOutlined"
 
 function App() {
   return (
@@ -10,10 +11,12 @@ function App() {
       <header className="Header">
         <h1 style={{}}>ZEK.GG</h1>
       </header>
+      <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<Projects />} />
         </Routes>
+      </main>
     </div>
   );
 }
@@ -21,7 +24,7 @@ function App() {
 function Home() {
   return (
     <>
-      <main className="main">
+      <div className="Home">
         <div className="mainLeft">
           <MainLogo className="mainLogo" />
         </div>
@@ -30,21 +33,37 @@ function Home() {
           {mainLink("/about", "TIMELINE")}
           {mainLink("/about", "CONTACT")}
         </div>
-      </main>
+      </div>
     </>
   );
 }
 
-function About() {
+function Projects() {
   return (
     <>
-      <main>
-        <h2>about this website</h2>
-        <p>this is a portfolio</p>
+      <main className="main">
+        <div className="ProjectList">
+          <a
+            className="ProjectCard"
+            href="https://github.com/themightyzek/scammus"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={projectImage} alt="has nothing to do with the project" />
+            <div className="Right">
+              <h1>Scammus</h1>
+              <p>
+                Twitch chat bot to act as a prediction platform. Lorem ipsum
+                dolor sit amet, lorem imsum. Loerntia salvatica eher sintra ran
+                do litiront, eve candirad hambug.
+              </p>
+              <div className="IconBar">
+                <OpenInNew/>
+              </div>
+            </div>
+          </a>
+        </div>
       </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
     </>
   );
 }
